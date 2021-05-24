eb80c71b6fd1e69c4bc4d4ea6f609253bb2ce831
FernandoPinage <luizfernandoluck@hotmail.com>
2021-05-24 17:32:44 -0400

    home ok

--- cdl/resources/views/home.blade.php
+++ cdl/resources/views/home.blade.php
@@ -1,16 +1,17 @@
 @extends('layout.home')
     
+       
         @section('conteudo')
 
         <form class="form-signin" method="POST" action="../home.php">
             <div class="text-center" id="text-pesquisar">
-                <p class="h1 text-center">Encontre a vaga ideal</p>
+                <p class="h1 text-center">Faça sua busca</p>
             </div>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Digite um cargo ou uma palavra chave" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <div class="input-group">
+                <input type="text" class="form-control form-control" placeholder="Digite um cargo ou uma palavra chave" aria-label="Recipient's username" aria-describedby="button-addon2">
                 <button class="btn btn-success" type="submit" id="button-addon2">Busca</button>
             </div>
         </form>  
         @endsection
-            
+        
    
