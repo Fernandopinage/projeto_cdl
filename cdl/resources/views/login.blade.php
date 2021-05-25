e08ae753bd1be1c0c754f4b77d49f5958ed326a0
FernandoPinage <luizfernandoluck@hotmail.com>
2021-05-25 10:21:14 -0400

    tela de login OMS

--- cdl/resources/views/login.blade.php
+++ cdl/resources/views/login.blade.php
@@ -1,7 +1,26 @@
 @extends('layout.app')
 
-@section('conteudo')
+@section('login')
     
-<h1>login</h1>
+<form class="form-signin" method="POST">
+    <div class="text-center" id="logo">
+        <h2 class="form-signin-heading">OM's</h2>
+        <hr>
+    </div>
+    <div class="mb-3">
+    <input type="text" class="form-control form-control-lg" name="login" placeholder="E-mail" required="" autofocus="" />
+    </div>
+    <div class="mb-3">
+    <input type="password" class="form-control form-control-lg" name="password" placeholder="Senha" required="" />
+    </div>
+    <div class="text-left">
+        <a class="nav-link" href="#">Esqueci minha senha</a>
+    </div>
+
+    <div class="d-grid gap-2">
+    <input type="submit" class="btn btn-lg" name="loginentrar" value="Entrar">
+    </div>
+
+</form>
 
 @endsection
