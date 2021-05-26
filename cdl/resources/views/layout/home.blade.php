13952efcf88da53c991d5f82e3b6aef5d8993edc
FernandoPinage <luizfernandoluck@hotmail.com>
2021-05-26 16:36:08 -0400

    menu ok

--- cdl/resources/views/layout/home.blade.php
+++ cdl/resources/views/layout/home.blade.php
@@ -59,11 +59,11 @@
                           UTV
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <li><a class="dropdown-item" href="login/utv">Entrar</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
-                          <li><hr class="dropdown-divider"></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                            <li><a class="dropdown-item" href="login/utv">Entrar</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                          </ul>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
