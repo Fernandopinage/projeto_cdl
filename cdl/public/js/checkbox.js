baf35ce59d32f460088a23dda436f89ee996f1a0
FernandoPinage <luizfernandoluck@hotmail.com>
2021-05-28 10:07:36 -0400

    js

---
+++ cdl/public/js/checkbox.js
@@ -0,0 +1,10 @@
+
+var checkboxPessoais = document.getElementById('pessoais');
+
+if(checkboxPessoais.checked) {
+    console.log("O cliente marcou o checkbox");
+} else {
+    console.log("O cliente não marcou o checkbox");
+}
+
+
--- cdl/resources/views/add_candidato.blade.php
+++ cdl/resources/views/add_candidato.blade.php
@@ -7,7 +7,7 @@
     <div class="form-signin">
         <div class="text-end">
             <label class="switch">
-                <input type="checkbox" checked>
+                <input type="checkbox" id="pessoais" checked>
                 <span class="slider round"></span>
             </label>
         </div>
@@ -17,82 +17,84 @@
             <hr>
         </div>
         
-            
-        <div class="row g-3">
+        <div class="pessoais">
 
-            <div class="col-md-4">
-                <label class="form-check-label" for="flexCheckIndeterminate">Primeiro nome </label>
-                <input type="text" class="form-control" placeholder="">
-            </div>
+            <div class="row g-3">
 
-            <div class="col-md-4">
-                <label class="form-check-label" for="flexCheckIndeterminate">Sobre Nome</label>
-                <input type="text" class="form-control" placeholder="">
-            </div>
-
-            <div class="col-md-4">
-                <label class="form-check-label" for="flexCheckIndeterminate">Selecione Sua foto</label>
-                <input class="form-control" type="file" id="formFile">
-              </div>  
-
-            <div class="col-md-3">
-                <label class="form-check-label" for="flexCheckIndeterminate">CPF</label>
-                <input type="text" class="form-control" placeholder="">
-            </div>
-            <div class="col-md-3">
-                <label class="form-check-label" for="flexCheckIndeterminate">RG</label>
-                <input type="text" class="form-control" placeholder="">
-            </div>
-            <div class="col-md-3">
-                <label class="form-check-label" for="flexCheckIndeterminate">Data Nascimento </label>
-                <input type="date" class="form-control" placeholder="">
-            </div>
-            <div class="col-md-3">
-                <label class="form-check-label" for="flexCheckIndeterminate">Local de Nascimento </label>
-                <input type="text" class="form-control" placeholder="">
-            </div>
-            <div class="col-md-2">
-                <label class="form-check-label" for="flexCheckIndeterminate">Cor</label>
-                <select id="inputState" class="form-select">
-                    <option selected></option>
-                    <option>Branca</option>
-                    <option>Preta</option>
-                    <option>Parta</option>
-                    <option>Amarela</option>
-                    <option>Indígena</option>
-                </select>
-            </div>
-            <div class="col-md-1">
-                <label class="form-check-label" for="flexCheckIndeterminate">Peso</label>
-                <input type="email" class="form-control" placeholder="">
-            </div>
-            <div class="col-md-1">
-                <label class="form-check-label" for="flexCheckIndeterminate">Altura</label>
-                <input type="email" class="form-control" placeholder="">
-            </div>
-            <div class="col-md-2">
-                <label class="form-check-label" for="flexCheckIndeterminate">Gênero </label>
-                <div class="form-check ">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                    <label class="form-check-label" for="gridRadios2">
-                    Homem
-                    </label>
+                <div class="col-md-4">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Primeiro nome </label>
+                    <input type="text" class="form-control" placeholder="">
                 </div>
-                <div class="form-check ">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
-                    <label class="form-check-label" for="gridRadios3">
-                    Mulher
-                    </label>
-                </div>
-                <div class="form-check ">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
-                    <label class="form-check-label" for="gridRadios3">
-                    Outros
-                    </label>
-                </div>
-            </div>
 
-        </div>   
+                <div class="col-md-4">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Sobre Nome</label>
+                    <input type="text" class="form-control" placeholder="">
+                </div>
+
+                <div class="col-md-4">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Selecione Sua foto</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>  
+
+                <div class="col-md-3">
+                    <label class="form-check-label" for="flexCheckIndeterminate">CPF</label>
+                    <input type="text" class="form-control" placeholder="">
+                </div>
+                <div class="col-md-3">
+                    <label class="form-check-label" for="flexCheckIndeterminate">RG</label>
+                    <input type="text" class="form-control" placeholder="">
+                </div>
+                <div class="col-md-3">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Data Nascimento </label>
+                    <input type="date" class="form-control" placeholder="">
+                </div>
+                <div class="col-md-3">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Local de Nascimento </label>
+                    <input type="text" class="form-control" placeholder="">
+                </div>
+                <div class="col-md-2">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Cor</label>
+                    <select id="inputState" class="form-select">
+                        <option selected></option>
+                        <option>Branca</option>
+                        <option>Preta</option>
+                        <option>Parta</option>
+                        <option>Amarela</option>
+                        <option>Indígena</option>
+                    </select>
+                </div>
+                <div class="col-md-1">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Peso</label>
+                    <input type="email" class="form-control" placeholder="">
+                </div>
+                <div class="col-md-1">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Altura</label>
+                    <input type="email" class="form-control" placeholder="">
+                </div>
+                <div class="col-md-2">
+                    <label class="form-check-label" for="flexCheckIndeterminate">Gênero </label>
+                    <div class="form-check ">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                        <label class="form-check-label" for="gridRadios2">
+                        Homem
+                        </label>
+                    </div>
+                    <div class="form-check ">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
+                        <label class="form-check-label" for="gridRadios3">
+                        Mulher
+                        </label>
+                    </div>
+                    <div class="form-check ">
+                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
+                        <label class="form-check-label" for="gridRadios3">
+                        Outros
+                        </label>
+                    </div>
+                </div>
+
+            </div>  
+        </div> 
     </div>
     <div class="form-signin">
         <div class="text-end">
