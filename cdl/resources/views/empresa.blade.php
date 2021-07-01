6da68eba1a087ddd713c44df741242310afc41e4
FernandoPinage <luizfernandoluck@hotmail.com>
2021-07-01 10:37:45 -0400

    atualização

--- cdl/resources/views/empresa.blade.php
+++ cdl/resources/views/empresa.blade.php
@@ -15,6 +15,10 @@
     <div class="mb-3">
     <input type="password" class="form-control form-control" name="password" placeholder="Senha"  />
     </div>
+    
+    <div class="d-grid gap-2">
+        <input type="submit" class="btn-primary btn-lg" name="loginentrar" value="Entrar">
+    </div>
     <div class="row g-3">
         <div class="col">
             <a class="nav-link" id="link1" href="/redefinir/empresa">Esqueci minha senha</a>
@@ -22,10 +26,6 @@
         <div class="col">
             <a class="nav-link" id="link1" href="/add/empresa">Criar uma conta</a>
         </div>
-    </div>
-
-    <div class="d-grid gap-2">
-    <input type="submit" class="btn-primary btn-lg" name="loginentrar" value="Entrar">
     </div>
 
 </form>
